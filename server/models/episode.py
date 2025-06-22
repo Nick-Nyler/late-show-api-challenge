092591fb88ac97744c0220065e23c6b8968351db
Nick <nicknyler@gmail.com>
2025-06-22 09:56:51 +0300

    added episode.py

---
+++ server/models/episode.py
@@ -0,0 +1,6 @@
+class Episode(db.Model):
+    __tablename__ = 'episodes'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+    appearances = db.relationship('Appearance', backref='episode', lazy=True, cascade="all, delete-orphan")
