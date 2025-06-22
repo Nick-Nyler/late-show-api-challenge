e174047dc98457ce1ef8acc932a5def2f6cd64e6
Nick <nicknyler@gmail.com>
2025-06-22 09:55:00 +0300

    added user.py

---
+++ server/config.py
@@ -0,0 +1,5 @@
+# server/config.py
+class Config:
+    SQLALCHEMY_DATABASE_URI = "postgresql://<user>:<password>@localhost:5432/late_show_db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "your-secret-key"  # Replace with a secure key
