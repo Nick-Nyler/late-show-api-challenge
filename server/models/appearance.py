3f427a06dac346d0eaecb3881cb93b2907727a35
Nick <nicknyler@gmail.com>
2025-06-22 09:57:50 +0300

    added appearance.py

---
+++ server/models/appearance.py
@@ -0,0 +1,10 @@
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+
+    __table_args__ = (
+        db.CheckConstraint('rating >= 1 AND rating <= 5', name='check_rating'),
+    )
